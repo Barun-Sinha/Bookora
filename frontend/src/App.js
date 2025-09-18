d68eab2b3631f5773972ddbb13d02b30ec32968f
Ishan Lomas <ishanlomas998@gmail.com>
2025-09-18 15:27:30 +0530

    setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,3 +9,6 @@ function App() {
 }
 
 export default App;
+
+
+// I push the code from main branch but now we have to do the change on stag branch so when i puh thecode in stag it goes to the owner of main branch he have right to push it to maain branch 
